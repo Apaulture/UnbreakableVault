0eeeb7ee008113b0772886da11eac86d225ed013
Paul Leelaviwatana <apaulture@gmail.com>
2020-01-25 11:34:16 -0800

    Initial Commit

---
+++ Break the Vault/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Break the Vault
+//
+//  Created by Apaulture on 1/25/20.
+//  Copyright © 2020 Apaulture. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
